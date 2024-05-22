629d98fe20269c29554242de74308406ce361df0
Aryan <aryan03pandey@gmail.com>
2024-05-22 13:46:48 +0530

    initial Commit

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+import '@styles/globals.css'
+
+//change the metadata of the application dynamically
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover and Share AI Prompts"
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html>
+        <body>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient' />
+                </div>            
+
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ components/Nav.jsx
@@ -0,0 +1,166 @@
+//since we are using client facing functionalities like hooks, we need to make it a client component
+"use client";
+
+//component imports
+import Link from "next/link"
+import Image from "next/image"
+
+//asset imports
+import logo from "../public/assets/images/logo.svg"
+
+//hooks and dependency imports
+import { useEffect, useState } from "react";
+import { signIn, signOut, getProviders, useSession } from "next-auth/react"
+
+const Nav = () => {
+
+  const { data: session } = useSession();
+
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  useEffect(() => {
+    const setUpProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setUpProviders();
+  }, [])
+
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center" >
+        <Image 
+          src={logo}
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+
+      {/* Desktop navigation */}
+      <div className="sm:flex hidden">
+        {
+          session ? 
+          <div className="flex gap-3 md:gap-3">
+            <Link 
+              href="/create-prompt"
+              className="black_btn"  
+            >
+              Create Post
+            </Link>
+
+            <button
+              type="button"
+              className="outline_btn"
+              onClick={() => signOut()}
+            >
+              SignOut
+            </button>
+
+            <Link href="/profile">
+              <Image 
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className="rounded-full "
+                alt="Profile"
+
+              />
+            </Link>
+          </div> 
+            : 
+          <>
+            {
+              providers && 
+              Object.values(providers).map((provider) => {
+                return <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              })
+            }
+          </>
+        }
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {
+          session?.user ? 
+          <div className="flex">
+            <Image 
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className="rounded-full "
+                alt="Profile"
+                onClick={() => setToggleDropdown((prevVal) => !prevVal)}
+              />
+
+            {
+              toggleDropdown && 
+              <div className="dropdown">
+                <Link 
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}  
+                > 
+                  My Profile
+                </Link>
+                <Link 
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}  
+                > 
+                  Create Prompt
+                </Link>
+
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+
+              </div>
+            }
+
+          </div>
+          :
+          <>
+            {
+              providers && 
+              Object.values(providers).map((provider) => {
+                return <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              })
+            }
+          </>
+        }
+
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
